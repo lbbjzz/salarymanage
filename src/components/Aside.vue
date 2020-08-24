620d6f56a2966da227caa22628f098e650ed23b1
xiaoyuou <942609405@qq.com>
2020-08-24 17:15:35 +0800

    头部以及侧边导航栏的初步设计与实现

---
+++ src/components/Aside.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-menu
+    :collapse="isCollapse"
+    :default-active="$route.path"
+    class="el-menu-vertical-demo"
+    background-color="#167bd8"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    style="height: 100vh;">
+      <el-submenu :index="1">
+          <template slot="title">
+              <span slot="title">首页</span>
+          </template>
+      </el-submenu>
+      <el-submenu :index="2">
+          <template slot="title">
+              <span slot="title">工资管理</span>
+          </template>
+      </el-submenu>
+      <el-submenu :index="3">
+          <template slot="title">
+              <span slot="title">员工管理</span>
+          </template>
+      </el-submenu>
+  </el-menu>
+</template>
+
+<style lang="scss" scoped>
+  .el-menu {
+        border: none;
+  }
+  .el-menu-vertical-demo {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+    <div class="header-left" @click="goIndex" v-bind:title="message">东软企业工资管理系统</div>
+    <div class="header-right">
+      <div>
+        <el-dropdown placement="bottom-start">
+          <img class="user-img" src="../assets/logo.png"/>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item @click.native="logOut">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      message: '回到首页'
+    }
+  },
+  methods: {
+    goIndex () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    logOut () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    background: linear-gradient(to right, #167bd8, #fff);
+    color: #fff;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+  }
+  .header-left {
+    font-size: 22px;
+    font-weight: bolder;
+    margin-left: 10px;
+    display: block;
+    cursor: pointer;
+  }
+  .user-img {
+    height: 45px;
+    width: 45px;
+  }
+  .header-right {
+    margin-left: 80%;
+    cursor: pointer;
+  }
+  .edittool {
+    display: none;
+  }
+  .header-left:hover+div{
+    display: block;
+    color: #000;
+  }
+</style>
