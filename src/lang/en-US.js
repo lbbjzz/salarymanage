e2ebac43fcaa422dc976e571b5a42fda03f2cbc9
lbbjzz <lbbjzz@gmail.com>
2020-08-25 11:24:27 +0800

    项目整合

--- src/lang/en-US.js
+++ src/lang/en-US.js
@@ -1,24 +1,10 @@
 export const language = {
-  title: 'Book borrowing management system',
-  email: 'Email',
-  phone: 'Phone',
+  title: 'SalaryManage System',
   userBox: 'Username',
   passwordBox: 'Password',
-  emailBox_error: 'Please Enter Your Email!',
-  phoneBox_error: 'Please Enter Your Phone Number!',
   userBox_error: 'Please Enter Your Username!',
   passwordBox_error: 'Please Enter Your Password',
-  login_success: 'login Successfully!',
-  login_error: 'login Failed！Please Check Your Username and Password!',
-  register_success: 'register Successfully!',
-  register_error: 'register Failed！Please Check Your Email、Username and Password!',
-  code_info_1: 'Done!',
-  code_info_2: 'Oops! Please Try Again!',
-  login: 'login',
-  register: 'register',
-  code: 'Code',
-  getCode: 'Get Code',
-  code_error: 'Please Enter Your Verify Code!',
-  getCode_error: 'Wrong Code!',
-  exits_error: 'UserName Exits!'
+  login_success: 'Login Successfully!',
+  login_error: 'Login Failed！Please Check Your Username and Password!',
+  login: 'Login'
 }
--- src/lang/zh-HK.js
+++ src/lang/zh-HK.js
@@ -1,24 +1,10 @@
 export const language = {
-  title: '圖書借閲管理系統',
-  email: '郵箱',
-  phone: '聯絡方式',
+  title: '企業薪資管理系統',
   userBox: '賬號',
   passwordBox: '密碼',
-  emailBox_error: '請輸入郵箱！',
   userBox_error: '請輸入賬號！',
   passwordBox_error: '請輸入密碼！',
-  phoneBox_error: '請輸入聯絡方式',
   login_success: '登錄成功！',
   login_error: '登錄失敗！请输入正确的用戶名或密碼！',
-  register_success: '注冊成功！',
-  register_error: '注冊失敗！請檢查的電子郵箱、用戶名或密碼！',
-  code_info_1: '校驗成功！',
-  code_info_2: '失敗，請重試！',
-  login: '登錄',
-  register: '注冊',
-  code: '驗證碼',
-  getCode: '獲取驗證碼',
-  code_error: '請輸入驗證碼！',
-  getCode_error: '驗證碼錯誤！',
-  exits_error: '用戶名存在！'
+  login: '登錄'
 }
