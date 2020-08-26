26c1674f4f426ac2394bb992a2eec9237c1f8bad
D <875708765@qq.com>
2020-08-26 11:25:35 +0800

    导入员工考勤数据

--- src/network/salaryManage/importData.js
+++ src/network/salaryManage/importData.js
@@ -1,13 +1,44 @@
+// eslint-disable-next-line import/no-duplicates
 import { request } from '@/network/request'
+// eslint-disable-next-line import/no-duplicates
+import { baseUrl } from '../request'
 
 // 获取导入的数据
-export function listImportVo (pageNo, pageSize) {
+export function listImportVo (pageNo, pageSize, time) {
   return request({
-    url: '/import/listImportVo',
-    method: 'get',
+    url: '/import/listImportVo/' + pageNo + '/' + pageSize + '/' + time,
+    method: 'get'
+  })
+}
+// 导入表格数据的url接口(会进行验证用户存在)
+export const uploadDataUrl = baseUrl + '/import/uploadData'
+
+// 导入表格数据的url接口(不会进行验证用户存在)
+export const uploadDataNotCheck = baseUrl + '/import/uploadDataNotCheck'
+
+// 更新员工考勤信息
+export function updateImport (id, sickLeaveDay, personalLeaveDay, lateDay, overtimeDay, backPay) {
+  return request({
+    url: '/import/updateImport',
+    method: 'post',
+    data: {
+      id,
+      sickLeaveDay,
+      personalLeaveDay,
+      lateDay,
+      overtimeDay,
+      backPay
+    }
+  })
+}
+
+// 删除员工考勤信息
+export function deleteImport (importId) {
+  return request({
+    url: '/import/deleteImport',
+    method: 'post',
     params: {
-      pageNo,
-      pageSize
+      importId
     }
   })
 }
--- src/views/salaryManage/importData/ImportData.vue
+++ src/views/salaryManage/importData/ImportData.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
+    <el-switch
+      @change="activeChange"
+      v-model="active"
+      active-text="上传时验证用户是否存在"
+      inactive-text="上传时不验证用户（速度快）">
+    </el-switch>
     <!--文件导入-->
-    <div style="margin-bottom: 50px">
+    <div style="margin-bottom: 50px;margin-top: 20px">
       <el-upload
         class="upload-demo"
         drag
-        action="https://jsonplaceholder.typicode.com/posts/"
+        :action="uploadUrl"
+        :on-success="uploadSuccess"
         accept=".xls, .xlsx"
       >
         <i class="el-icon-upload"></i>
@@ -17,94 +24,148 @@
     <!--表格-->
     <div>
       <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>{{ time }}----导入员工考勤数据</span>
+        </div>
         <el-table
-          :data="tableData"
+          :data="importVoList"
           stripe
           style="width: 100%">
           <el-table-column
-            prop="date"
-            label="Id">
+            prop="id"
+            label="Id"
+            width="50">
           </el-table-column>
           <el-table-column
-            prop="name"
+            prop="employeeName"
             label="员工姓名">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="employeeDept"
             label="员工部门">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="employeeJob"
             label="员工岗位">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="sickLeaveDay"
             label="病假天数">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="personalLeaveDay"
             label="事假天数">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="lateDay"
             label="迟到次数">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="overtimeDay"
             label="加班天数">
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="backPay"
             label="补发工资">
-          </el-table-column>
+          </el-table-column>s
           <el-table-column
-            prop="address"
-            label="创建时间">
+            prop="createTime"
+            label="创建时间"
+            width="200">
           </el-table-column>
           <el-table-column
             label="操作">
             <template slot-scope="scope">
-              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-              <el-button type="text" size="small">编辑</el-button>
+              <el-button @click="editImport(scope.row)" type="text" size="small">编辑</el-button>
+              <el-divider direction="vertical"></el-divider>
+              <el-popconfirm
+                icon="el-icon-info"
+                iconColor="red"
+                @onConfirm="deleted(scope.row)"
+                title="是否刪除该员工考勤数据？"
+              >
+                <el-button slot="reference" type="text" size="small" style="color: red">删除</el-button>
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
       </el-card>
+      <!--编辑的表单-->
+      <el-dialog title="编辑员工考勤数据" :visible.sync="dialogFormVisible">
+        <el-form :model="importData">
+          <el-form-item label="员工姓名：">
+            <el-input :readOnly="true" v-model="importData.employeeName"></el-input>
+          </el-form-item>
+          <el-form-item label="部门：">
+            <el-input :readOnly="true" v-model="importData.employeeDept"></el-input>
+          </el-form-item>
+          <el-form-item label="岗位：">
+            <el-input :readOnly="true" v-model="importData.employeeJob"></el-input>
+          </el-form-item>
+          <el-form-item label="病假天数：">
+            <el-input-number :step=1 :step-strictly="true" v-model="importData.sickLeaveDay" :min="0" :max="31"></el-input-number>
+          </el-form-item>
+          <el-form-item label="事假天数：">
+            <el-input-number :step=1 :step-strictly="true" v-model="importData.personalLeaveDay" :min="0" :max="31"></el-input-number>
+          </el-form-item>
+          <el-form-item label="迟到天数：">
+            <el-input-number :step=1 :step-strictly="true" v-model="importData.lateDay" :min="0" :max="31"></el-input-number>
+          </el-form-item>
+          <el-form-item label="加班天数：">
+            <el-input-number :step="1" :step-strictly="true" v-model="importData.overtimeDay" :min="0" :max="31"></el-input-number>
+          </el-form-item>
+          <el-form-item label="补发工资：">
+            <el-input onkeyup='this.value=this.value.replace(/\D/gi,"")' v-model="importData.backPay"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="updateImportData">确 定</el-button>
+        </div>
+      </el-dialog>
     </div>
     <!--表格-->
+    <!--分页-->
+    <div style="float: right;margin-top: 30px;margin-bottom: 20px;margin-right: 60px">
+      <el-pagination
+        background
+        :current-page="pageNo"
+        :page-size="pageSize"
+        @current-change="pageNoChange"
+        layout="prev, pager, next"
+        :total="total">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-import { listImportVo } from '../../../network/salaryManage/importData'
+import { listImportVo, uploadDataUrl, uploadDataNotCheck, updateImport, deleteImport } from '../../../network/salaryManage/importData'
 
 export default {
   name: 'ImportData',
   data () {
     return {
+      time: '',
       pageNo: 1,
       pageSize: 10,
       // 表格数据
       importVoList: [],
       total: 0,
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      // 导入数据的接口
+      uploadUrl: uploadDataUrl,
+      uploadDataUrl: uploadDataUrl,
+      uploadDataNotCheck: uploadDataNotCheck,
+      dialogFormVisible: false,
+      // 点击编辑导入的某条数据
+      importData: {},
+      labelPosition: 'right',
+      active: true
     }
+  },
+  created () {
+    const date = new Date()
+    this.time = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日'
   },
   mounted () {
     this.getImportVoList()
@@ -112,13 +173,71 @@ export default {
   methods: {
     // eslint-disable-next-line vue/no-dupe-keys
     getImportVoList () {
-      listImportVo(this.pageNo, this.pageSize).then(res => {
+      listImportVo(this.pageNo, this.pageSize, 'now').then(res => {
         if (res.code === 2000) {
           this.importVoList = res.data.listImportVo
           this.total = res.data.total
         }
         console.log(res)
       })
+    },
+    // 文件上传成功
+    uploadSuccess () {
+      this.getImportVoList()
+    },
+    // 编辑导入数据的表单打开
+    editImport (row) {
+      this.importData = row
+      this.dialogFormVisible = true
+    },
+    // 更新员工导入的数据
+    updateImportData () {
+      updateImport(this.importData.id, this.importData.sickLeaveDay,
+        this.importData.personalLeaveDay, this.importData.lateDay,
+        this.importData.overtimeDay, this.importData.backPay).then(res => {
+        this.dialogFormVisible = false
+        if (res.code === 2000) {
+          this.$message({
+            message: '修改成功！',
+            type: 'success'
+          })
+          return
+        }
+        this.$message({
+          message: '修改失败！',
+          type: 'error'
+        })
+      })
+    },
+    // 删除员工考勤数据
+    deleted (row) {
+      deleteImport(row.id).then(res => {
+        if (res.code === 2000) {
+          this.$message({
+            message: '删除成功！',
+            type: 'success'
+          })
+          this.getImportVoList()
+          return
+        }
+        this.$message({
+          message: '删除失败！',
+          type: 'error'
+        })
+      })
+    },
+    // 页号改变
+    pageNoChange (pageNo) {
+      this.pageNo = pageNo
+      this.getImportVoList()
+    },
+    // 开关状态变化
+    activeChange (flag) {
+      if (flag === false) {
+        this.uploadUrl = this.uploadDataNotCheck
+      } else {
+        this.uploadUrl = this.uploadDataUrl
+      }
     }
   }
 }
